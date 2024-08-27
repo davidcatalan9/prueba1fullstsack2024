d808d4d6fa655a43ac21cfa05d1113213d8e76c8
davidcatalan9 <dcatalan@gmail.com>
2024-08-26 21:26:09 -0400

    aprendiendo git

---
+++ assets/js/script.js
@@ -0,0 +1,38 @@
+console.log('estamos conectado1 !!!');
+
+const usuario1 = {
+    nombre: 'david',
+    apellido: 'catalan',
+    edad: 41,
+    rol: 'alumno',
+    curso: 'fullstack js',
+    numero_rut: '1215454544',
+};
+
+usuario1.edad = 23;
+
+console.log(usuario1.edad);
+
+const listasuper = ['azucar', 'pan', 'mermelada', { producto: 'huevos', cantidad: 24 }];
+
+// console.log(listasuper);
+// for (let index = 0; index < listasuper.length; index++) {
+//     // const element = listasuper[index];
+//     // console.log(element);
+//     console.log(listasuper[index]);
+// }
+// console.log(usuario2)
+//
+// for (let index in listasuper) {
+// console.log(key, usuario1[key]);
+// console.log(index, listasuper[index]);
+// }
+//
+// let comprasObjeto = [];
+// for (let index of listasuper) {
+//     const objeto1 = { producto: index, cant: 5 };
+//     comprasObjeto.push(objeto1);
+//     console.log(index);
+// }
+// console.log(comprasObjeto);
+
